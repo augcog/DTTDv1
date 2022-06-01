f2c815b5ed5d1ad832cc21009b2a57585fc1240b
Adam Chang <adamchang2000@berkeley.edu>
2022-06-01 15:04:52 -0700

    have some form of icp init

--- manual_pose_annotation/ManualPoseAnnotator.py
+++ manual_pose_annotation/ManualPoseAnnotator.py
@@ -12,6 +12,7 @@ import numpy as np
 import cv2
 import open3d as o3d
 import matplotlib.pyplot as plt
+from manual_pose_annotation.depth_utils import fill_missing
 
 class ManualPoseAnnotator:
 
@@ -23,8 +24,7 @@ class ManualPoseAnnotator:
         self.objects = objects
         self.camera_intrinsic_matrix = camera_intrinsic_matrix
         self.camera_distortion_coefficients = camera_distortion_coefficients
-
-
+        
     @staticmethod
     def pointcloud_from_rgb_depth(rgb, depth, depth_scale, intrinsic, distortion):
 
@@ -69,28 +69,69 @@ class ManualPoseAnnotator:
     @staticmethod
     def icp_pose_initializer(rgb, depth, depth_scale, camera_intrinsic_matrix, camera_distortion_coefficients, objects):
         
+        depth_smoothed = fill_missing(depth, 1 / depth_scale, 1)
+
         camera_pcld = ManualPoseAnnotator.pointcloud_from_rgb_depth(rgb, depth, depth_scale, camera_intrinsic_matrix, camera_distortion_coefficients)
+        smoothed_camera_pcld = ManualPoseAnnotator.pointcloud_from_rgb_depth(rgb, depth_smoothed, depth_scale, camera_intrinsic_matrix, camera_distortion_coefficients)
 
-        print('Please select the approximate center of the object in the scene')
-        plt.imshow(rgb)
-        p1, p2 = plt.ginput(2)
-        plt.close()
-            
+        print('Please select the approximate center of each object in the scene')
 
-        return np.array([np.eye(4) for i in range(len(objects))])
+        object_centers = {}
+
+        for obj_id, obj_data in objects.items():
+            print("Select the center for object named {0}".format(obj_data["name"]))
+
+            plt.imshow(rgb)
+            x, y = plt.ginput(1)[0]
+            plt.close()
+
+            object_centers[obj_id] = np.array(smoothed_camera_pcld.points)[int(y * depth.shape[1] + x)]
+
+        object_pose_initializations = {}
+
+        for obj_id in object_centers.keys():
+            obj_mesh = objects[obj_id]["mesh"]
+            obj_center = object_centers[obj_id]
+
+            obj_mesh_with_normals = obj_mesh.compute_vertex_normals()
+
+            target_vertices = obj_mesh_with_normals.vertices
+            target_normals = obj_mesh_with_normals.vertex_normals
+
+            target_pcld = o3d.geometry.PointCloud()
+            target_pcld.points = target_vertices
+            target_pcld.normals = target_normals
+
+            trans_init = np.eye(4)
+            trans_init[:3,3] = obj_center
+
+            transform_obj_to_scene = o3d.registration.registration_icp(
+                target_pcld, camera_pcld, .05, trans_init,
+                o3d.registration.TransformationEstimationPointToPoint())
+
+            print("icp result for obj {0}".format(obj_id))
+            print(transform_obj_to_scene)
+
+            transform_obj_to_scene = transform_obj_to_scene.transformation
+
+            object_pose_initializations[obj_id] = transform_obj_to_scene
+
+        return object_pose_initializations
 
     """
     rgb and depth are images
     depth_scale is the scale of the depth image, we multiply depth * depth_scale to get depth in meters
     initialization method is a function that takes in rgb, depth, depth scale, camera intrinsic matrix, 
     camera distortion coefficients, and list of object triangle meshes
-    and returns a numpy array of size (Ox4x4), the affine transform for each object
+    and returns a dict {obj_id: transform initialization}, the affine transform for each object
     """
     def annotate_pose(self, rgb, depth, depth_scale, initialization_method=None):
         if initialization_method:
             initial_poses = initialization_method(rgb, depth, depth_scale, self.camera_intrinsic_matrix, self.camera_distortion_coefficients, self.objects)
         else:
-            initial_poses = np.array([np.eye(4) for i in range(len(self.objects))])
+            initial_poses = {}
+            for obj_id in self.objects.keys():
+                initial_poses[obj_id] = np.eye(4)
 
         #TODO: Pose Annotator here
         annotated_poses = initial_poses
