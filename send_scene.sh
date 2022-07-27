6ffcb6f0378c3331710479b9ca7d64cb3bd1f093
Adam Chang <adamchang2000@berkeley.edu>
2022-07-27 15:24:29 -0700

    send_scene to windows server

--- send_scene.sh
+++ send_scene.sh
@@ -1,1 +1,1 @@
-rsync -axv /cygdrive/d/adam/6d-pose-dataset/scenes/$1 openark@192.168.0.163:/ssd/adam/6d-pose-dataset/scenes/ --exclude "data_raw"
+rsync -axv /cygdrive/d/adam/6d-pose-dataset/scenes/$1 openark@192.168.0.184:/ssd/adam/6d-pose-dataset/scenes/ --exclude "data_raw"
