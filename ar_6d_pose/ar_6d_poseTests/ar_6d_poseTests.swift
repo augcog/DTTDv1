88862f341818c08a49b181748876b0a1fdc99691
Weiyu Feng <47650296+0WeiyuFeng0@users.noreply.github.com>
2022-07-29 16:08:10 -0700

    iphone side done

---
+++ ar_6d_pose/ar_6d_poseTests/ar_6d_poseTests.swift
@@ -0,0 +1,36 @@
+//
+//  ar_6d_poseTests.swift
+//  ar_6d_poseTests
+//
+//  Created by Weiyu Feng on 7/17/22.
+//
+
+import XCTest
+@testable import ar_6d_pose
+
+class ar_6d_poseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
