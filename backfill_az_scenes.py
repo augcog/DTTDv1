ceda1720e780562f0340709706a682a1f338c1b6
Adam <adamchang2000@berkeley.edu>
2022-08-01 18:07:46 -0700

    backfills intrinsic and frame metas for az scenes

--- backfill_az_scenes.py
+++ backfill_az_scenes.py
@@ -1,3 +1,4 @@
+import json
 import os
 from utils.camera_utils import load_intrinsic_static, write_static_intrinsic
 
@@ -11,6 +12,22 @@ for scene in os.listdir("scenes"):
         continue
     write_static_intrinsic(camera_name, scene_dir, raw=False)
     write_static_intrinsic(camera_name, scene_dir, raw=True)
+
+    data = os.path.join(scene_dir, "data")
+
+    for f in os.listdir(data):
+        f = os.path.join(data, f)
+        if "meta" not in f:
+            continue
+            
+        with open(f, "r") as file:
+            meta = json.load(file)
+
+        meta["intrinsic"] = camera_intr.tolist()
+
+        with open(f, "w") as file:
+            meta = json.dump(meta, file)
+
 
 for scene in os.listdir("extrinsics_scenes"):
     scene_dir = os.path.join("scenes", scene)
