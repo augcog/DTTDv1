d37e9ef40c480b814732854429b91266e3b33da3
Adam Chang <adamchang2000@berkeley.edu>
2022-08-12 16:11:24 -0700

    test aruco with raw data

--- testing/test_aruco.py
+++ testing/test_aruco.py
@@ -1,9 +1,10 @@
 import argparse
 from collections import defaultdict
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
-import open3d as o3d
 from pyk4a import PyK4A, CalibrationType
+from scipy.spatial.transform import Rotation as R
 import yaml
 
 import os, sys 
@@ -11,8 +12,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dir_path, ".."))
 
 from utils.camera_utils import load_frame_intrinsics, load_frame_distortions
-from utils.frame_utils import calculate_aruco_from_bgr_and_depth, load_bgr, load_depth, load_rgb
-from utils.pointcloud_utils import pointcloud_from_rgb_depth
+from utils.frame_utils import calculate_aruco_from_bgr_and_depth, load_bgr, load_depth, load_rgb, get_color_ext
 
 '''
 X - Red
@@ -23,6 +23,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Test ARUCO.')
     parser.add_argument('--scene_dir', default='', type=str)
+    parser.add_argument('--raw', action='store_true')
 
     args = parser.parse_args()
 
@@ -40,8 +41,12 @@ if __name__ == "__main__":
         os.mkdir(output_dir)
 
     if args.scene_dir:
-        frame_dir = os.path.join(args.scene_dir, "data")
-        frame_ext = "png"
+        if args.raw:
+            frame_dir = os.path.join(args.scene_dir, "data_raw")
+            frame_ext = get_color_ext(frame_dir)
+        else:
+            frame_dir = os.path.join(args.scene_dir, "data")
+            frame_ext = "png"
 
         scene_metadata_file = os.path.join(args.scene_dir, "scene_meta.yaml")
         with open(scene_metadata_file, 'r') as file:
@@ -49,8 +54,8 @@ if __name__ == "__main__":
 
         camera_name = scene_metadata["camera"]
         cam_scale = scene_metadata["cam_scale"]
-        camera_intrinsics_dict = load_frame_intrinsics(args.scene_dir, raw=False)
-        camera_dists_dict = load_frame_distortions(args.scene_dir, raw=False)
+        camera_intrinsics_dict = load_frame_intrinsics(args.scene_dir, raw=args.raw)
+        camera_dists_dict = load_frame_distortions(args.scene_dir, raw=args.raw)
 
     else:
         # Modify camera configuration
@@ -60,6 +65,9 @@ if __name__ == "__main__":
         cam_scale = 0.001
         camera_matrix = k4a.calibration.get_camera_matrix(CalibrationType.COLOR)
         camera_dists_dict = defaultdict(lambda: k4a.calibration.get_distortion_coefficients(CalibrationType.COLOR))
+
+    translations = []
+    rotations = []
 
     while True:
         if not args.scene_dir:
@@ -74,7 +82,6 @@ if __name__ == "__main__":
             rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
         else:
             try:
-                frame_dir = os.path.join(args.scene_dir, "data")
                 color_image = load_bgr(frame_dir, img_id, frame_ext)
                 rgb = load_rgb(frame_dir, img_id, frame_ext)
                 depth_image = load_depth(frame_dir, img_id)
@@ -100,12 +107,45 @@ if __name__ == "__main__":
             cv2.aruco.drawDetectedMarkers(color_image, corners)  # Draw A square around the markers
             cv2.aruco.drawAxis(color_image, camera_matrix, camera_dists_dict[img_id], rvec, tvec / 9, 0.01)  # Draw Axis
 
+            rotmat = R.from_rotvec(rvec).as_matrix()
+            rotations.append(rotmat.squeeze())
+            translations.append(tvec)
+
             # Display the resulting frame
             cv2.imshow("image", color_image)
 
         img_id += 1
 
+        print("Frame: {0}".format(img_id), end='\r')
+
         # Press q key to stop
         if cv2.waitKey(1) == ord('q'):
             break
-    
+
+    good_poses = [0]
+    rotation_diffs = []
+    translation_diffs = []
+
+    for idx in range(1, len(rotations)):
+
+        rot_curr = rotations[idx]
+
+        if np.sum(rot_curr) == 0:
+            continue
+
+        rotation_diff = np.arccos((np.trace(rot_curr @ np.linalg.inv(rotations[0])) - 1.) / 2.)
+        translation_diff = np.linalg.norm(translations[idx] - translations[0])
+        
+        if np.isnan(rotation_diff):
+            rotation_diff = 1.
+
+        rotation_diffs.append(rotation_diff)
+        translation_diffs.append(translation_diff)
+
+    print("number of good ARUCO detections!", len(good_poses))
+
+    plt.plot(np.arange(0, len(rotation_diffs)), rotation_diffs, label="regular")
+    plt.legend()
+    plt.show()
+
+
